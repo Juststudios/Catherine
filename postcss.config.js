3edeb9b630327a3e6c088f26dbba80e665a0e180
Jephthah vegba <Jephthahvegba17@gmail.com>
2025-11-15 19:00:08 +0000

    m

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
+
+
+// =========================
+// src/main.jsx
+// =========================
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
